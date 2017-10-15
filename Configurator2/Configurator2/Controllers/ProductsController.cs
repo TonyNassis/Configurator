1b9188becd27315a5078520b8a0ac099d98f5674
tonyskilos <tonyskilos@gmail.com>
2017-10-15 14:44:17 +0200

    Hit a problem

---
+++ Configurator2/Configurator2/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Configurator2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Configurator2.Controllers {
+	public class ProductsController : Controller {
+
+		//--- set up the context for the controller
+		private BRMO_Integration_Context _context;
+
+		public ProductsController() {
+			_context = new BRMO_Integration_Context();
+		}
+
+		protected override void Dispose(bool disposing) {
+			_context.Dispose();
+		}
+		//------------------------------------------------
+
+		public ActionResult Index() {
+
+			var products = _context.products.ToList();
+
+				
+			return View(products);
+		}
+	}
+}
