297884646ec32c4133501bcdc0b87442f9bc254b
tonyskilos <tonyskilos@gmail.com>
2017-10-16 00:01:57 +0200

    Added StockpileForm

--- Configurator2/Configurator2/Controllers/StockpilesController.cs
+++ Configurator2/Configurator2/Controllers/StockpilesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Configurator2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,63 @@ namespace Configurator2.Controllers {
 			var stockpiles = _context.Stockpiles.ToList();
 
 			return View(stockpiles);
+		}
+
+		public ActionResult Edit(int id) {
+
+			var stockpile = _context.Stockpiles.SingleOrDefault(m => m.StockpileIdentifier == id);
+
+			if (stockpile == null) {
+				return HttpNotFound();
+			}
+
+			var viewModel = new StockpileFormViewModel {
+				StockpileIdentifier = stockpile.StockpileIdentifier,
+				StockpileName = stockpile.StockpileName,
+				StockpileCapacity =  stockpile.StockpileCapacity,
+				StockpileServiceTypeIdentifier = stockpile.StockpileServiceTypeIdentifier,
+				StockpileStartLimit = stockpile.StockpileStartLimit ,
+				StockpileCentreLimit = stockpile.StockpileCentreLimit ,
+				StockpileEndLimit = stockpile.StockpileEndLimit ,
+				PlannedBuildProductIdentifier = stockpile.PlannedBuildProductIdentifier,
+				BuildAngleOfRepose = stockpile.BuildAngleOfRepose ,
+				StackerNo = stockpile.StackerNo ,
+				ReclaimerQuadrant = stockpile.ReclaimerQuadrant ,
+				StartPositionOffset  = stockpile.StartPositionOffset ,
+				StartLuffingAngle  = stockpile.StartLuffingAngle ,
+				ConeShellStepDistance = stockpile.ConeShellStepDistance ,
+				BenchHeight  = stockpile.BenchHeight ,
+				BenchVolume  = stockpile.BenchVolume ,
+				InitialLuffAngle  = stockpile.InitialLuffAngle ,
+				InitialLongTravelPosition = stockpile.InitialLongTravelPosition,
+				StackingRegimeIdentifier = stockpile.StackingRegimeIdentifier ,
+				StackingFormulaIdentifier = stockpile.StackingFormulaIdentifier ,
+				BLStartStackingDirection = stockpile.BLStartStackingDirection ,
+				BLStartReclaimingDirection = stockpile.BLStartReclaimingDirection ,
+				Bench1LuffAngle = stockpile.Bench1LuffAngle ,
+				ParBench1SlewAngle  = stockpile.ParBench1SlewAngle ,
+				Bench1LongTravelPosition = stockpile.Bench1LongTravelPosition,
+				Bench2LuffAngle = stockpile.Bench2LuffAngle ,
+				Bench2SlewAngle  = stockpile.Bench2SlewAngle ,
+				Bench2LongTravelPosition = stockpile.Bench2LongTravelPosition,
+				Bench3LuffAngle = stockpile.Bench3LuffAngle ,
+				Bench3SlewAngle = stockpile.Bench3SlewAngle ,
+				Bench3LongTravelPosition = stockpile.Bench3LongTravelPosition,
+				StockpileLength = stockpile.StockpileLength ,
+				LongTravelStepLength = stockpile.LongTravelStepLength ,
+				PilgrimStepLength = stockpile.PilgrimStepLength ,
+				InnerSlewLimit = stockpile.InnerSlewLimit ,
+				OuterSlewLimit = stockpile.OuterSlewLimit ,
+				InnerSlewTurnaroundLimit = stockpile.InnerSlewTurnaroundLimit,
+				OuterSlewTurnaroundLimit = stockpile.OuterSlewTurnaroundLimit ,
+				ReclaimRateSetpoint  = stockpile.ReclaimRateSetpoint ,
+				InitialSlewAngle  = stockpile.InitialSlewAngle ,
+				InitialSlewInnerTurnaroundLimit = stockpile.InitialSlewInnerTurnaroundLimit,
+				InitialSlewOuterTurnaroundLimit = stockpile.InitialSlewOuterTurnaroundLimit ,
+			};
+
+			return View("StockpileForm", viewModel);
+
 		}
 	}
 }
---
+++ Configurator2/Configurator2/ViewModels/StockpileFormViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Configurator2.ViewModels {
+
+	public class StockpileFormViewModel {
+
+
+		public int StockpileIdentifier { get; set; }
+		public string StockpileName { get; set; }
+		public double? StockpileCapacity { get; set; }
+		public int StockpileServiceTypeIdentifier { get; set; }
+		public double? StockpileStartLimit { get; set; }
+		public double? StockpileCentreLimit { get; set; }
+		public double? StockpileEndLimit { get; set; }
+		public int PlannedBuildProductIdentifier { get; set; }
+		public double? BuildAngleOfRepose { get; set; }
+		public int? StackerNo { get; set; }
+		public int? ReclaimerQuadrant { get; set; }
+		public double? StartPositionOffset { get; set; }
+		public double? StartLuffingAngle { get; set; }
+		public double? ConeShellStepDistance { get; set; }
+		public double? BenchHeight { get; set; }
+		public double? BenchVolume { get; set; }
+		public double? InitialLuffAngle { get; set; }
+		public double? InitialLongTravelPosition { get; set; }
+		public int? StackingRegimeIdentifier { get; set; }
+		public bool? StackingFormulaIdentifier { get; set; }
+		public bool? BLStartStackingDirection { get; set; }
+		public bool? BLStartReclaimingDirection { get; set; }
+		public double? Bench1LuffAngle { get; set; }
+		public double? ParBench1SlewAngle { get; set; }
+		public double? Bench1LongTravelPosition { get; set; }
+		public double? Bench2LuffAngle { get; set; }
+		public double? Bench2SlewAngle { get; set; }
+		public double? Bench2LongTravelPosition { get; set; }
+		public double? Bench3LuffAngle { get; set; }
+		public double? Bench3SlewAngle { get; set; }
+		public double? Bench3LongTravelPosition { get; set; }
+		public double? StockpileLength { get; set; }
+		public double? LongTravelStepLength { get; set; }
+		public double? PilgrimStepLength { get; set; }
+		public double? InnerSlewLimit { get; set; }
+		public double? OuterSlewLimit { get; set; }
+		public double? InnerSlewTurnaroundLimit { get; set; }
+		public double? OuterSlewTurnaroundLimit { get; set; }
+		public double? ReclaimRateSetpoint { get; set; }
+		public double? InitialSlewAngle { get; set; }
+		public double? InitialSlewInnerTurnaroundLimit { get; set; }
+		public double? InitialSlewOuterTurnaroundLimit { get; set; }
+	}
+}
