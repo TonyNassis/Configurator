cb92e28222792409ed1a697275ef36010ebfde42
tonyskilos <tonyskilos@gmail.com>
2017-10-15 22:54:08 +0200

    Stockpile Controller & Index View

---
+++ Configurator2/Configurator2/Controllers/StockpilesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Configurator2.Controllers {
+
+	public class StockpilesController : Controller {
+
+		//--- set up the context for the controller
+		private BRMO_Integration_Context _context;
+
+		public StockpilesController() {
+			_context = new BRMO_Integration_Context();
+		}
+
+		protected override void Dispose(bool disposing) {
+			_context.Dispose();
+		}
+		//------------------------------------------------
+
+		public ActionResult Index() {
+
+			var stockpiles = _context.Stockpiles.ToList();
+
+			return View(stockpiles);
+		}
+	}
+}
