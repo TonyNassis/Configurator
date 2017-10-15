bb31805a074cf1fbb2c1c45b2dadb9aaa41fc167
tonyskilos <tonyskilos@gmail.com>
2017-10-15 21:39:11 +0200

    Added edit product

--- Configurator2/Configurator2/Controllers/ProductsController.cs
+++ Configurator2/Configurator2/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Configurator2.Models;
+using Configurator2.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,8 +25,39 @@ namespace Configurator2.Controllers {
 
 			var products = _context.products.ToList();
 
-				
 			return View(products);
+		}
+
+		public ActionResult Edit(int id) {
+
+			var product = _context.products.SingleOrDefault(m => m.ProductIdentifier == id);
+
+			if (product == null) {
+				return HttpNotFound();
+			}
+
+			var viewModel = new ProductFormViewModel {
+				ProductIdentifier = product.ProductIdentifier,
+				ProductName = product.ProductName,
+				ParPercentageBlending = product.ParPercentageBlending
+			};
+
+			return View("ProductForm", viewModel);
+		}
+
+		[HttpPost]
+		public ActionResult Save(ProductFormViewModel product) {
+
+			if (product.ProductIdentifier == 0) {
+				return HttpNotFound();
+			} else {
+				var productInDb = _context.products.Single(m => m.ProductIdentifier == product.ProductIdentifier);
+				productInDb.ParPercentageBlending = product.ParPercentageBlending;
+
+				_context.SaveChanges();
+
+				return RedirectToAction("Index", "Products");
+			}
 		}
 	}
 }
---
+++ Configurator2/Configurator2/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Configurator2.ViewModels {
+
+	public class ProductFormViewModel {
+		public int ProductIdentifier { get; set; }
+		public string ProductName { get; set; }
+		public double? ParPercentageBlending { get; set; }
+	}
+}
